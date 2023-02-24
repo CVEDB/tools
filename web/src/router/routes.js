5c322f3377f0d00dab669484774486d77c7f863f
Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>
2023-02-24 15:23:03 -0800

    Create routes.js
    Signed-off-by: Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>

---
+++ web/src/router/routes.js
@@ -0,0 +1,21 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: 'home', component: () => import('pages/Index.vue') },
+      { path: ':id(CVEDB-\\d{4}-\\d{4,})', component: () => import('pages/Identifier.vue') }
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/Error404.vue')
+  }
+]
+
+export default routes
